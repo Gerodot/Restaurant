d56c095211134e9e89ca07e8bb5420ba89e93fba
Gerodot <gerodot@me.com>
2022-06-06 12:16:00 +0300

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Gerodot on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
