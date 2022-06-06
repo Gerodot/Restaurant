afdcc7e196f8e439990895772986c6ac11f32e43
Gerodot <gerodot@me.com>
2022-06-06 17:36:21 +0300

    Added images in item table view

--- Restaurant/Manager/CellManager.swift
+++ Restaurant/Manager/CellManager.swift
@@ -9,12 +9,30 @@ import Foundation
 import UIKit
  
 class CellManager {
+    let networkManager = NetworkManager()
+    
     func configure(_ cell: UITableViewCell, with category: String) {
         cell.textLabel?.text = category.localizedCapitalized
     }
 
-    func configure(_ cell: UITableViewCell, with menuItem: MenuItem) {
+    func configure(
+        _ cell: UITableViewCell,
+        with menuItem: MenuItem,
+        for tableView: UITableView? = nil,
+        indexPath: IndexPath
+    ) {
         cell.textLabel?.text = menuItem.name
         cell.detailTextLabel?.text = String(format: "$%.2f", menuItem.price)
+       
+        guard cell.imageView?.image == nil else { return }
+        networkManager.getImage(menuItem.imageURL) { image, error in
+            if let error = error {
+                print(#line, #function, error.localizedDescription)
+            }
+            DispatchQueue.main.async {
+                cell.imageView?.image = image
+                tableView?.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
     }
 }
