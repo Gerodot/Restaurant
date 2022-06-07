bd92813a2307c9e3590229a3b9d20092b89f3022
Gerodot <gerodot@me.com>
2022-06-07 21:28:57 +0300

    fixed image load and update images

--- Restaurant/Models/MenuItem.swift
+++ Restaurant/Models/MenuItem.swift
@@ -6,15 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
-struct MenuItem: Codable {
+class MenuItem: Codable {
     let id: Int
     var name: String
     var detailText: String
     var price: Double
     var category: String
     var imageURL: URL
+    var image: UIImage?
 
+    init(
+        id: Int,
+        name: String,
+        detailText: String,
+        price: Double,
+        category: String,
+        imageURL: URL,
+        image: UIImage? = nil
+    ) {
+        self.id = id
+        self.name = name
+        self.detailText = detailText
+        self.price = price
+        self.category = category
+        self.imageURL = imageURL
+        self.image = image
+    }
+    
     enum CodingKeys: String, CodingKey {
         case id
         case name
